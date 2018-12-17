8c7ec7f56640271f8adae393808417d94b83e292
WweiZhang <604297964@qq.com>
2018-12-17 15:52:48 +0800

    12-17

---
+++ 第十二次作业/coreDateDemo/coreDateDemo/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  coreDateDemo
+//
+//  Created by student on 2018/12/13.
+//  Copyright © 2018年 2016110452. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfName: UITextField!
+    @IBOutlet weak var tfPhone: UITextField!
+    
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //print(NSHomeDirectory())
+    }
+
+    @IBAction func add(_ sender: Any) {
+        let person = Person(context: context)
+        person.name = tfName.text
+        person.phone = tfPhone.text
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    //    @IBAction func update(_ sender: Any) {
+//        let fetch:NSFetchRequest<Person> = Person.fetchRequest()
+//        fetch.predicate = NSPredicate(format: "name=%@", tfName.text!)
+//        let persons = try? context.fetch(fetch)
+//        if let p = persons?.first {
+//            p.phone = tfPhone.text
+//        }
+//    }
+//
+//    @IBAction func del(_ sender: Any) {
+//        let fetch:NSFetchRequest<Person> = Person.fetchRequest()
+//        fetch.predicate = NSPredicate(format: "name=%@", tfName.text!)
+//   4¥     let persons = try? context.fetch(fetch)
+//        if let p = persons?.first {
+//            context.delete(p)
+//        }
+//    }
+//
+//    @IBAction func query(_ sender: Any) {
+//        let fetch:NSFetchRequest<Person> = Person.fetchRequest()
+//        fetch.predicate = NSPredicate(format: "name=%@", tfName.text!)
+//        let persons = try? context.fetch(fetch)
+//        if let p = persons?.first {
+//            tfPhone.text = p.phone
+//        }
+//    }
+}
+
